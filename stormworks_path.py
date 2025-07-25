96946ff6e0d3b4ab7677fdb0625c3054eece5c4d
lganic <55366941+lganic@users.noreply.github.com>
2025-07-25 14:36:27 -0400

    Delete stormworks_path.py

--- stormworks_path.py
+++
@@ -1,5 +0,0 @@
-STORMWORKS_PATH = None # Edit this to be something like: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Stormworks" (include the quotes)
-
-
-if STORMWORKS_PATH is None:
-    raise NotImplementedError('Please edit stormworks_path.py to add the path to your Stormworks installation folder. It most likely looks something like: C:\\Program Files (x86)\\Steam\\steamapps\\common\\Stormworks')
